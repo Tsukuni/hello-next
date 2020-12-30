c5ed5dc081fc35c21d74feed82eb163a7de28897
Tsukuni <tamkchi.fugu@gmail.com>
2020-12-30 21:49:49 +0900

    first commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { PrismaClient } from "@prisma/client";
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    //　プロバイダーは何個でも指定できる。
+    // https://next-auth.js.org/getting-started/introduction で一覧がみれる
+    // 例：
+    // Providers.Twitter({
+    //   clientId: process.env.TWITTER_CLIENT_ID,
+    //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    // }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+  callbacks: {
+    session: async (session, user) => {
+      session.user.id = user.id;
+
+      return Promise.resolve(session);
+    }
+  }
+};
+
+export default (req, res) => NextAuth(req, res, options);
---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { signIn, signOut, useSession } from "next-auth/client";
+import TodoList from "../components/TodoList";
+import NewTodoForm from "../components/NewTodoForm";
+
+export default function Home() {
+  const [session, loading] = useSession();
+
+  return (
+    <>
+      {!session && (
+        <>
+          サインインしてください。<br />
+          <button onClick={signIn}>signIn</button>
+        </>
+      )}
+      { session && (
+        <>
+          サインイン完了。email: {session.user.email} <br />
+          <button onClick={signOut}>Sign out</button>
+          <NewTodoForm />
+          <TodoList />
+        </>
+      )}
+    </>
+  )
+}
